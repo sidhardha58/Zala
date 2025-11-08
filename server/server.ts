7696e18dbc7e1e4c2da1ca3e11e6283130122ec2
sidhardha58 <sidhu.valavala@gmail.com>
2025-11-08 11:39:58 +0530

    Fixed Errors

--- server/server.ts
+++ server/server.ts
@@ -15,7 +15,6 @@ const allowedOrigins = [
   "http://localhost:5173",
   "http://localhost:8080",
   "https://zala-8639a.firebaseapp.com",
-  "https://zala-cyan.vercel.app",
   process.env.DOMAIN, // Production frontend
 ].filter(Boolean);
 
